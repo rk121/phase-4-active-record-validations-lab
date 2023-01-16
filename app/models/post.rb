6ae6e5392b6c0ad0cdc086507c5267342544f303
rk121 <roshankhadka613@gmail.com>
2023-01-16 19:55:06 +1100

    commit tree eff8a7581dddbfa5f41bee80ba7eff215b6cab5e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+    validates :title_is_not_click_bait
+
+    def title_is_not_click_bait
+        title_must_include = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        if !title_must_include.any? { |word| title.include?(word) }
+            errors.add(:title, "Not click bait-y enough!")
+        end
+    end
 end
